ffb39a45796afab8d4396b90a5d5ea3d64eeb0bc
Sayaka Nogawa <hello@iamsaya.com>
2020-04-15 01:37:59 +0900

    changed content blade templete

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -2,6 +2,9 @@
   <header>
     <h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
     @include('partials/entry-meta')
+    <picture>
+      <a href="{{ get_permalink() }}">{!! get_the_post_thumbnail() !!}</a>
+    </picture>
   </header>
   <div class="entry-summary">
     @php the_excerpt() @endphp
