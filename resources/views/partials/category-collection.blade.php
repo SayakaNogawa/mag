6fcd7e3d8bd3966356b4730bba607b685344ed7c
Sayaka Nogawa <hello@iamsaya.com>
2020-04-15 01:23:02 +0900

    add collection section and style

---
+++ resources/views/partials/category-collection.blade.php
@@ -0,0 +1,7 @@
+<div class="collection">
+  <div class="collection__head">
+    <h1 class="collection__title title">Magazine collection</h1>
+    <p class="collection__discription discription-japanese">ショップスタッフがオススメする特別なアイテムを紹介します</p>  
+  </div>
+  <button class="button" type="button">Read More</button>
+</div>
