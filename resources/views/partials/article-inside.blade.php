d0c1464bc404a664ad910e0ac4b3012bbd6b014f
Sayaka Nogawa <hello@iamsaya.com>
2020-04-16 23:52:44 +0900

    add genelic article html

---
+++ resources/views/partials/article-inside.blade.php
@@ -0,0 +1,14 @@
+<article @php post_class() @endphp>
+  <div>
+    <picture>
+      <a href="{{ get_permalink() }}">{!! get_the_post_thumbnail() !!}</a>
+    </picture>
+  </div>
+  <div>
+    <h2><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
+    <div>
+      @php the_excerpt() @endphp
+      @include('partials/entry-meta')
+    </div>
+  </div>
+</article>
