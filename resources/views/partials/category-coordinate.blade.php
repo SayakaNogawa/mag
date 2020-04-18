3559b6af419e747b7682fce49fbc57866fcc0563
Sayaka Nogawa <hello@iamsaya.com>
2020-04-18 23:59:53 +0900

    change coordinate blade

--- resources/views/partials/category-coordinate.blade.php
+++ resources/views/partials/category-coordinate.blade.php
@@ -1,22 +1,26 @@
-<h1 class="title">Coordinate</h1>
 <div class="slider">
-  <div class="slider__inner">
-
-    @php $the_query = set_query('coordinate', 7) @endphp
-    @while ($the_query->have_posts()) @php $the_query->the_post() @endphp
-
-      @if ($the_query->current_post == 0)
-        <div class="slider__single slider__single--right">
-          @include('partials.article-inside')
-        </div>
-        <div class="slider__columns slider__single--left">
-      @else
-        @include('partials.article-inside')
-      @endif
-    
-    @endwhile
+  <div class="container slider__container">
+    <div class="slider__head-container container-flex-row">
+      <h1 class="title--big slider__title slider__title--left">Coordinate</h1>
+      <p class="slider__readmore">READ MORE</p>
     </div>
-    @php wp_reset_postdata() @endphp
+    <div class="slider__inner">
+      @php $the_query = set_category_query('coordinate', 7) @endphp
+      @while ($the_query->have_posts()) @php $the_query->the_post() @endphp
 
+        @if ($the_query->current_post == 0)
+          <div class="slider__single slider__single--right">
+            @include('partials.article-inside')
+          </div>
+          <div class="slider__columns slider__single--left">
+        @else
+          @include('partials.article-inside')
+        @endif
+      
+      @endwhile
+      </div>
+      @php wp_reset_postdata() @endphp
+
+    </div>
   </div>
 </div>
