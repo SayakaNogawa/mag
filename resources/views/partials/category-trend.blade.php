b0c37146f7d0529730f007202b422926c7cb69a8
Sayaka Nogawa <hello@iamsaya.com>
2020-04-19 00:01:05 +0900

    change category blade

--- resources/views/partials/category-trend.blade.php
+++ resources/views/partials/category-trend.blade.php
@@ -1,11 +1,11 @@
-<div class="trend">
-  <h1 class="title">Trend</h1>
-  <div class="slider">
+<div class="slider">
+  <div class="container slider__container">
+    <h1 class="title--big slider__title slider__title--right">Trend</h1>
     <div class="slider__inner">
-      @php $trend_query = set_query('trend', 7) @endphp
-      @while ($trend_query->have_posts()) @php $trend_query->the_post() @endphp
+      @php $the_query = set_category_query('trend', 7) @endphp
+      @while ($the_query->have_posts()) @php $the_query->the_post() @endphp
 
-        @if ($trend_query->current_post == 0)
+        @if ($the_query->current_post == 0)
           <div class="slider__single slider__single--left">
             @include('partials.article-inside')
           </div>
