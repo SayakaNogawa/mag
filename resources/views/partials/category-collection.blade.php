cb0326a1dc8cd625aeb67b8d08fcf691c15ad4db
Sayaka Nogawa <hello@iamsaya.com>
2020-04-18 23:58:53 +0900

    change collection blade

--- resources/views/partials/category-collection.blade.php
+++ resources/views/partials/category-collection.blade.php
@@ -1,7 +1,15 @@
-<div class="collection">
-  <div class="collection__head">
-    <h1 class="collection__title title">Magazine collection</h1>
-    <p class="collection__discription discription-japanese">ショップスタッフがオススメする特別なアイテムを紹介します</p>  
+<div class="single-sec">
+  <div class="container single-sec__container">
+    <div class="single-sec__head">
+      <h1 class="title single-sec__title">Magazine collection</h1>
+      <p class="discription-japanese single-sec__discription">ショップスタッフがオススメする特別なアイテムを紹介します</p>  
+    </div>
+    <div class="single-sec__readmore-container">
+      @php $category_id = get_cat_ID('collection') @endphp
+      <a href="{{ get_category_link($category_id) }}">
+        <span href="" class="single-sec__readmore" type="button">Read More</span>
+        <img src="@asset('images/arrow-right.png')" alt="arrow-right" class="single-sec__icon">
+      </a>
+    </div>
   </div>
-  <button class="button" type="button">Read More</button>
 </div>
