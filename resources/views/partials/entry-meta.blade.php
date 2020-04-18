a90f9237f0e23af2c63bfce2cf797ec81a8bc87b
Sayaka Nogawa <hello@iamsaya.com>
2020-04-19 00:18:11 +0900

    change entry-meta and layout

--- resources/views/partials/entry-meta.blade.php
+++ resources/views/partials/entry-meta.blade.php
@@ -1,6 +1,13 @@
-<time class="updated" datetime="{{ get_post_time('c', true) }}">{{ get_the_date() }}</time>
-<p class="byline author vcard">
-  {{ __('By', 'sage') }} <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
-    {{ get_the_author() }}
-  </a>
-</p>
+<div class="entry-meta">
+  <span>
+    <ul class="entry-meta__category-container">
+      <li class="entry-meta__category">{{ the_category(', ') }}</li>
+    </ul>
+    <time class="entry-meta__updated" datetime="{{ get_post_time('c', true) }}">{{ the_time('Y.m.d') }}</time>
+    <p class="byline author vcard">
+    {{ __('Words By', 'sage') }} <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
+      {{ get_the_author() }}
+    </a>
+    </p>
+  </span>
+</div>
