a9006a4e6dbd4de412b59cb1f416cf402809c82b
Sayaka Nogawa <hello@iamsaya.com>
2020-04-19 21:13:22 +0900

    change entry-meta html

--- resources/views/partials/entry-meta.blade.php
+++ resources/views/partials/entry-meta.blade.php
@@ -4,10 +4,13 @@
       <li class="entry-meta__category">{{ the_category(', ') }}</li>
     </ul>
     <time class="entry-meta__updated" datetime="{{ get_post_time('c', true) }}">{{ the_time('Y.m.d') }}</time>
-    <p class="byline author vcard">
-    {{ __('Words By', 'sage') }} <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
+    <p class="entry-meta__author">
+    {{ __('記事を書いた人：', 'sage') }} <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
       {{ get_the_author() }}
     </a>
     </p>
+    <ul class="entry-meta__tag-container">
+      <li class="entry-meta__tag">{{ get_the_tags(', ') }}</li>
+    </ul>
   </span>
 </div>
