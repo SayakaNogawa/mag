5459fa4d6fbbdced880f726993392231d8653b97
Sayaka Nogawa <hello@iamsaya.com>
2020-04-19 22:03:25 +0900

    make footer responsive

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,49 +1,48 @@
 <footer class="footer">
-  <div class="footer__container">
-    <div class="footer__avobe-container container-flex-row">
-      <h2 class="footer__title title--small">magazine</h2>
-        <div class="footer__below-container">
-          <ul class="footer__menu">
-            <li class="footer__menu-item">
-              <a href="">Company</a>
-            </li>
-            <li class="footer__menu-text">
-              <a href="">Terms of Service</a>
-            </li>
-            <li class="footer__menu-text">
-              <a href="">Privacy Policy</a>
-            </li>
-            <li class="footer__menu-text">
-              <a href="">Help</a>
-            </li>
-            <li class="footer__menu-text">
-              <a href="">特商法について</a>
-            </li>
-            <li class="footer__menu-text">
-              <a href="">使い方ガイド</a>
-            </li>
-            <li class="footer-menu-img">
-              <span class="footer-social-twitter">
-                <a href="" onclick="javascript:window.open(''); return false;">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="13.502" height="10.971" viewBox="0 0 13.502 10.971">
-                    <g id="Group_71335" data-name="Group 71335" transform="translate(-1325.123 -792.31)">
-                      <path id="Path_40034" data-name="Path 40034" d="M79.246,109.421a7.828,7.828,0,0,0,7.882-7.882c0-.12,0-.239-.008-.358A5.637,5.637,0,0,0,88.5,99.748a5.529,5.529,0,0,1-1.591.436,2.78,2.78,0,0,0,1.218-1.532,5.551,5.551,0,0,1-1.759.672,2.773,2.773,0,0,0-4.721,2.526,7.865,7.865,0,0,1-5.709-2.894,2.772,2.772,0,0,0,.858,3.7,2.75,2.75,0,0,1-1.257-.347v.035a2.771,2.771,0,0,0,2.222,2.716,2.766,2.766,0,0,1-1.251.048,2.773,2.773,0,0,0,2.588,1.924,5.558,5.558,0,0,1-3.44,1.188,5.639,5.639,0,0,1-.659-.04,7.843,7.843,0,0,0,4.246,1.242" transform="translate(1250.123 693.86)"></path>
-                    </g>
-                  </svg>
-                </a>
-              </span>
-              <span class="footer-social-facebook">
-                <a href="" onclick="javascript:window.open(''); return false;">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="7.465" height="13.582" viewBox="0 0 7.465 13.582"> <g id="Group_71333" data-name="Group 71333" transform="translate(-1303.05 -791.418)">
-                    <path id="Path_40032" data-name="Path 40032" d="M11.546,19V12.8H9.461V10.389h2.086V8.612a2.948,2.948,0,0,1,2.6-3.186,17.374,17.374,0,0,1,2.368.089v2.16H15.236c-.508,0-1.192.212-1.192.765v1.949h2.391L16.12,12.8H14.04V19Z" transform="translate(1294 786)">
-                      </path>
-                    </g>
-                  </svg>
-                </a>
-              </span>
-            </li>
-          </ul>
-    </div>
+  <div class="footer__container container-flex-row">
+    <h2 class="footer__title title--small">magazine</h2>
+    <span>
+      <ul class="footer__menu">
+        <li class="footer__menu-item">
+          <a href="">Company</a>
+        </li>
+        <li class="footer__menu-item">
+          <a href="">Terms of Service</a>
+        </li>
+        <li class="footer__menu-item">
+          <a href="">Privacy Policy</a>
+        </li>
+        <li class="footer__menu-item">
+          <a href="">Help</a>
+        </li>
+        <li class="footer__menu-item">
+          <a href="">特商法について</a>
+        </li>
+        <li class="footer__menu-item">
+          <a href="">使い方ガイド</a>
+        </li>
+        <li class="footer__menu-item">
+          <span class="footer-twitter">
+            <a href="" onclick="javascript:window.open(''); return false;">
+              <svg xmlns="http://www.w3.org/2000/svg" width="13.502" height="10.971" viewBox="0 0 13.502 10.971">
+                <g id="Group_71335" data-name="Group 71335" transform="translate(-1325.123 -792.31)">
+                  <path d="M79.246,109.421a7.828,7.828,0,0,0,7.882-7.882c0-.12,0-.239-.008-.358A5.637,5.637,0,0,0,88.5,99.748a5.529,5.529,0,0,1-1.591.436,2.78,2.78,0,0,0,1.218-1.532,5.551,5.551,0,0,1-1.759.672,2.773,2.773,0,0,0-4.721,2.526,7.865,7.865,0,0,1-5.709-2.894,2.772,2.772,0,0,0,.858,3.7,2.75,2.75,0,0,1-1.257-.347v.035a2.771,2.771,0,0,0,2.222,2.716,2.766,2.766,0,0,1-1.251.048,2.773,2.773,0,0,0,2.588,1.924,5.558,5.558,0,0,1-3.44,1.188,5.639,5.639,0,0,1-.659-.04,7.843,7.843,0,0,0,4.246,1.242" transform="translate(1250.123 693.86)"></path>
+                </g>
+              </svg>
+            </a>
+          </span>
+          <span class="footer__facebook">
+            <a href="" onclick="javascript:window.open(''); return false;">
+              <svg xmlns="http://www.w3.org/2000/svg" width="7.465" height="13.582" viewBox="0 0 7.465 13.582"> <g id="Group_71333" data-name="Group 71333" transform="translate(-1303.05 -791.418)">
+                <path d="M11.546,19V12.8H9.461V10.389h2.086V8.612a2.948,2.948,0,0,1,2.6-3.186,17.374,17.374,0,0,1,2.368.089v2.16H15.236c-.508,0-1.192.212-1.192.765v1.949h2.391L16.12,12.8H14.04V19Z" transform="translate(1294 786)">
+                  </path>
+                </g>
+              </svg>
+            </a>
+          </span>
+        </li>
+      </ul>
+    </span>
   </div>
   <div class="footer__copyright-container container-flex-row">
     <a href="https://www.adobe.com/index2.html" onclick="javascript:window.open('https://www.adobe.com/index2.html'); return false;">
