bf96e8a0102ad1b20dd52f082201aa56d65c4e76
Sayaka Nogawa <hello@iamsaya.com>
2020-04-19 21:12:41 +0900

    change recommend and layout

--- resources/views/partials/recommend.blade.php
+++ resources/views/partials/recommend.blade.php
@@ -4,39 +4,36 @@
   <div class="container recommend__container">
 
     <div class="recommend__line">
-      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="172.5px" height="172.5px"><path fill-rule="evenodd"  stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(0, 0, 0)" d="M171.500,1.500 L1.500,171.500 "/></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="172.5px" height="172.5px"><path fill-rule="evenodd"  stroke="#bcbebf" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="#bcbebf" d="M171.500,1.500 L1.500,171.500 "/></svg>
     </div>
 
     <div class="recommend__article-container">
-      <div class="recommend__section-title">
+      <div class="recommend__title-contsiner">
         <h1 class="title recommend__title">Recommended Content</h1>
-        <h2 class="title--small recommend__subtitle">
-          Recommended article<br >
-          based on the article you red
-        </h2>
       </div>
       <div class="recommend__posts-wrapper">
         @while($the_query->have_posts()) @php $the_query->the_post() @endphp
-          <ul>
-            <li class="recommend__post">
-              @include('partials.entry-meta')
-                @if (has_post_thumbnail())
-                  <div class="recommend__post_thumbnail">
-                    <a href="{{ the_permalink() }}" title="{{ the_title_attribute() }}">
-                    {{ the_post_thumbnail() }}
-                    </a>
-                  </div>
-                @endif
-                <h3 class="recommend__post-title" >
-                  <a href="{{ the_permalink() }}" title="{{ the_title_attribute() }}">
-                    {{ the_title() }}
-                  </a>
-                </h3>
-            </li>
-          </ul>
+          <article class="recommend__post">
+            @if (has_post_thumbnail())
+              <div class="recommend__thumbnail">
+                <a href="{{ the_permalink() }}" title="{{ the_title_attribute() }}">
+                {{ the_post_thumbnail() }}
+                </a>
+              </div>
+            @endif
+            <span class="recommend__category-container">
+              <ul><li class="entry-meta__category recommend__category">{{ the_category(', ') }}</li></ul>
+            </span>
+            <h2 class="article-title-japanese recommend__post-title" >
+              <a href="{{ the_permalink() }}" title="{{ the_title_attribute() }}">
+                {{ the_title() }}
+              </a>
+            </h2>
+            <div class="discription-japanese recommend__excerpt">{{ the_excerpt() }}</div>
+          </article>
         @endwhile
         @php wp_reset_postdata() @endphp
-      </div>  
+      </div>
     </div>
   </div>
 </article>
