1c393d4586763403184b009177f6d26f8dad089c
Sayaka Nogawa <hello@iamsaya.com>
2020-04-20 18:26:18 +0900

    add site menu

---
+++ resources/views/partials/site-menu.blade.php
@@ -0,0 +1,19 @@
+<div class="site-menu">
+  <div class="site-menu__container">
+    <nav>
+      <ul>
+        <div class="site-menu__item-container">
+          <li><a href="" class="site-menu__item">Q&A</a></li>
+          <li><a href="" class="site-menu__item">Items</a></li>
+          <li><a href="" class="site-menu__item">Ranking</a></li>
+          <li><a href="" class="site-menu__item">Articles</a></li>
+          <li><a href="" class="site-menu__item">Shops</a></li>
+        </div>
+      </ul>
+    </nav>
+    <div class="site-menu__button-wrapper">
+      <button class="button">login</button>
+      <button class="button japanese">出店する</button>
+    </div>
+  </div>
+</div>
